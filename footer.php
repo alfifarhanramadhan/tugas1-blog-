b345fe74aaba9aeda0918df370b3adabea0365fd
alfifarhanramadhan <aanramadhan261014@gmail.com>
2020-06-19 20:00:24 +0700

    upload pertama

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div class="container wrap">
+    <div class="row">
+        <div class="col-md-6">
+            <p>Telepon, WA: <tel>085368721002</tel>,<br> Email: <b>aanramadhan261014@gmail.com</b></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+            <p>Hak Cipta &copy; <?php  echo Date("Y"); ?> Cerita Bola</p>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="container wrap">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="">
+
+                <div class="deskripsi">
+                    <h1><b><font color="#fff">Cerita Bola</font></b></h1>
+                    <p><font color="#fff">Baca ceritanya disini....</font></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
